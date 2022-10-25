ac8baa088bd124fcffd468f1475ff21696ba5eec
Nil Meneses <108699665+gnilfm@users.noreply.github.com>
2022-10-25 12:30:37 -0300

    configuraçao banco de dados

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,18 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import type { respostaPadraoMsg } from '../../types/respostaPadraoMsg'
 
-export default(
+const endpointLogin = (
     req : NextApiRequest,
-    res : NextApiResponse
+    res : NextApiResponse<respostaPadraoMsg>
 ) => {
     if(req.method === "POST"){
         const {login, senha} = req.body
 
         if (login === "admin@admin.com" && 
             senha === "admin@123"){
-                res.status(200).json({msg : "Ususario autenticado com sucesso"});
+               return res.status(200).json({msg : "Ususario autenticado com sucesso"});
     }
     return res.status(405).json({erro : "Ususario ou senha não encontrado"});
     
 }
     return res.status(405).json({erro : "Metodo informado não é valido"});
 }
+export default conectarMongoDB(endpointLogin);
