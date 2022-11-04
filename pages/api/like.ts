3d00ed41b0848dcd8a895f1459ff7fab2b9ffab4
Nil Meneses <108699665+gnilfm@users.noreply.github.com>
2022-11-04 20:52:31 -0300

    api like/deslike

---
+++ pages/api/like.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { validarTokenJwt } from "../../middlewares/validarTokenJWT";
+import { publicacaoModel } from "../../models/publicacaoModel";
+import { usuarioModel } from "../../models/usuarioModel";
+import type {respostaPadraoMsg} from "../../types/respostaPadraoMsg";
+
+const likeEndpoint
+ = async (req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>)=> {
+
+try{
+
+    if(req.method === "PUT"){
+
+        const {id} = req?.query;
+        const publicacao = await publicacaoModel.findById(id);
+        if(!publicacao) {
+            return res.status(400).json({erro :"Publicação nao encontrada"});
+        }
+        const {userId} = req?.query;
+        const usuario = await usuarioModel.findById(userId);
+        if(!usuario) { 
+            return res.status(400).json({erro :"Usuario não encontrado"});
+        }
+       
+        const indexDoUsuarioNoLike = publicacao.likes.findIndex((e : any) => e.toString() === usuario._id.toString());
+
+        if(indexDoUsuarioNoLike != -1){
+            publicacao.likes.splice(indexDoUsuarioNoLike, 1);
+            await publicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao); 
+            return res.status(200).json({msg : "Pblicacao descurtida com sucesso"});
+        }else{
+            publicacao.likes.push(usuario._id);  
+            await publicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao); 
+            return res.status(200).json({msg : "Pblicacao curtida com sucesso"});
+        }
+    }
+    return res.status(405).json({erro : "Metodo informado nao e valido"});
+
+}catch(e){
+    console.log(e);
+}
+return res.status(500).json({erro : "Ocorreu err ao curtir/descutir publicacao"});
+
+}
+
+export default validarTokenJwt(conectarMongoDB(likeEndpoint));
--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -9,18 +9,26 @@ const pesquisaEndpoint
 
             try{
                 if(req.method === "GET"){  
-
-                    const {filtro} = req.query;
-                    if(!filtro || filtro.length < 2){
-                        return res.status(400).json({erro: "Favor informar pelo menos 2 caracteres para busca"}); 
-                }
-        const usuariosEncontrados = await usuarioModel.find({
-            $or :[{nome : {$regex : filtro, $options: "i"}},
-                //{email : {$regex : filtro, $options: "i"}}
-            ]
-        });
-        return res.status(200).json(usuariosEncontrados);
-                    
+                    if(req?.query?.id){
+                        const usuarioEncontrado = await usuarioModel.findById(req?.query?.id);
+                        if(!usuarioEncontrado) {
+                            return res.status(400).json({ erro :"Usuario nao encontrado"});                                    
+                        }
+                        usuarioEncontrado.senha = null;
+                         return res.status(200).json(usuarioEncontrado);
+        
+                    }else{
+                        const {filtro} = req.query;
+                        if(!filtro || filtro.length < 2){
+                            return res.status(400).json({erro: "Favor informar pelo menos 2 caracteres para busca"}); 
+                    }
+            const usuariosEncontrados = await usuarioModel.find({
+                $or :[{nome : {$regex : filtro, $options: "i"}},
+                    //{email : {$regex : filtro, $options: "i"}}
+                ]
+            });
+            return res.status(200).json(usuariosEncontrados);
+        }                    
      }
      return res.status(405).json({erro: "Metodo informado nao e valido"});              
            
